36add052cf3be9937fad3963e5b81ea4a6e0de73
audrie <audrie.talbot@gmail.com>
2015-05-27 13:44:15 -0700

    Pass first two specs.

--- lib/fo_shizzle.rb
+++ lib/fo_shizzle.rb
@@ -6,20 +6,23 @@ class String
 
     word_array.each() do |word|
       storage_array = []
+      index = 0
       letter_array = word.split("")
 
         letter_array.each() do |letter|
-          if letter.==('s')
+          if letter.==('s') && index != (0)
             letter.replace('z')
             storage_array.push(letter)
           else
             storage_array.push(letter)
+
           end
+          index = index + 1
         end # end letter loop
 
         phrase_array.push(storage_array.join(""))
     end # end word loop
-    
+
     phrase_array.join(" ")
   end
 end
--- spec/fo_shizzle_spec.rb
+++ spec/fo_shizzle_spec.rb
@@ -4,6 +4,9 @@ require('fo_shizzle')
 
 describe('fo_shizzle') do
   it("changes the letter 's' to 'z'") do
-    expect(("sally sells seashells").fo_shizzle()).to(eq("zally zellz zeazhellz"))
+    expect(("molly eats hamburgers").fo_shizzle()).to(eq("molly eatz hamburgerz"))
+  end
+  it("does not change the letter 's' if it is the first letter of a word") do
+    expect(("ssally sells seashells").fo_shizzle()).to(eq("szally sellz seazhellz"))
   end
 end
